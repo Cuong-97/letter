91f326a41449a6021e7c82bb9e57b7b91c50c5e5
C210102 <c210102@ccg.ac.jp>
2022-12-05 01:22:41 +0900

    upload_mp3

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const modal = document.querySelector('.js_modal');
 
-
-// click bên ngoài modal vẫn có thể đóng
 modal.addEventListener('click', function(){
     modal.classList.remove('close');
 });
